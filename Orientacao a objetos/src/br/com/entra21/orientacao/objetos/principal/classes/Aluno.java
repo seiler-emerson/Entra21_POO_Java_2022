0b505a78a43a75f96b49a7c12f940c03e6b57c13
seiler-emerson <seiler.emerson@gmail.com>
2022-05-20 20:01:10 -0300

    Class 01 - OOP

---
+++ Orientacao a objetos/src/br/com/entra21/orientacao/objetos/principal/classes/Aluno.java
@@ -0,0 +1,17 @@
+package br.com.entra21.orientacao.objetos.principal.classes;
+
+public class Aluno {
+	// definir atributos de um aluno e cada objeto aluno tera essas variaveis, chamadas de atributos
+	public String nome;
+	public byte idade;
+	public byte quantidadePresencas;
+	public byte notas[][];
+	
+	// na declaracao só tem, acesso e tipo de retorno
+	public Aluno() { //construtor vazio que ao utilizar o (new Aluno) ele é executado
+		
+	}
+	
+	
+	
+}
